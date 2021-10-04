52f3e1c121d8ba84b26f3711b21cf5eaee7281fc
Kieran Wood <kieranw098@gmail.com>
2021-10-04 11:00:34 -0600

    Removed embedded python folder

--- Languages/Python/Embedded Python for windows testing/install.bat
+++
@@ -1,8 +0,0 @@
-python-files\python.exe python-files\get-pip.py
-echo =================================== && echo "pip installed moving onto jupyter" && echo ===================================
-python-files\python.exe -m pip install jupyterlab
-python-files\python.exe -m pip install ipywidgets && python-files\python.exe -m jupyter labextension install @jupyter-widgets/jupyterlab-manager@2.0
-python-files\python.exe -m pip install ipycanvas && python-files\python.exe -m jupyter labextension install @jupyter-widgets/jupyterlab-manager ipycanvas
-python-files\python.exe -m pip install ipyevents && python-files\python.exe -m jupyter labextension install @jupyter-widgets/jupyterlab-manager ipyevents
-python-files\python.exe -m pip install .\python-files\spark-main
-python-files\python.exe -m jupyter notebook
